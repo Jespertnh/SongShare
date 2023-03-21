d114c6d76823ae035756a38593dc556c8e2ca9e9
jespertnh <jespertenholder@gmail.com>
2023-03-21 16:27:44 +0100

    UserController gebruikt nu ApiResponse

--- src/main/java/nl/jesper/songshare/controllers/UserController.java
+++ src/main/java/nl/jesper/songshare/controllers/UserController.java
@@ -1,17 +1,16 @@
 package nl.jesper.songshare.controllers;
 
-import nl.jesper.songshare.exceptions.UsernameAlreadyExistsException;
 import nl.jesper.songshare.dto.requests.post.UserRegistrationRequest;
-import nl.jesper.songshare.dto.responses.BadResponse;
-import nl.jesper.songshare.dto.responses.SuccessResponse;
+import nl.jesper.songshare.dto.responses.ApiResponse;
+import nl.jesper.songshare.exceptions.UsernameAlreadyExistsException;
 import nl.jesper.songshare.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-
 
 /**
  * Controller for handling HTTP requests regarding user accounts.
@@ -27,15 +26,15 @@ public class UserController {
     }
 
     @PostMapping(path = "/users/register", produces = MediaType.APPLICATION_JSON_VALUE)
-    ResponseEntity<?> registerUser(@RequestBody UserRegistrationRequest request) {
+    public ResponseEntity<ApiResponse> registerUser(@RequestBody UserRegistrationRequest request) {
         try {
             userService.createUser(request.getUsername(), request.getPassword());
-//            return ResponseEntity.ok("Success registering user");
-            return ResponseEntity.ok(new SuccessResponse("Registered user successfully."));
-        } catch (UsernameAlreadyExistsException usernameAlreadyExistsException) {
-            return ResponseEntity.badRequest().body(new BadResponse("Username '" + request.getUsername() + "' already exists."));
+            return ResponseEntity.ok(new ApiResponse(HttpStatus.OK.value(), "Registered user successfully."));
+        } catch (UsernameAlreadyExistsException e) {
+            return ResponseEntity.badRequest().body(new ApiResponse(HttpStatus.BAD_REQUEST.value(), "Username '" + request.getUsername() + "' already exists."));
+        } catch (Exception e) {
+            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(new ApiResponse(HttpStatus.INTERNAL_SERVER_ERROR.value(), "An error occurred while processing your request."));
         }
     }
-
 
 }
