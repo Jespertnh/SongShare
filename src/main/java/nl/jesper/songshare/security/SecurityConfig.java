7c7235b87b36de26b70d61a24bdf08456e19cbd2
jespertnh <jespertenholder@gmail.com>
2023-03-23 10:16:38 +0100

    Geprobeerd dit te volgen: https://docs.spring.io/spring-security/reference/servlet/authentication/passwords/jdbc.html
    maar geeft error

--- src/main/java/nl/jesper/songshare/security/SecurityConfig.java
+++ src/main/java/nl/jesper/songshare/security/SecurityConfig.java
@@ -2,13 +2,27 @@ package nl.jesper.songshare.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
 import org.springframework.security.crypto.factory.PasswordEncoderFactories;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import static org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType.H2;
 
 @Configuration
 public class SecurityConfig {
@@ -30,6 +44,32 @@ public class SecurityConfig {
 //                .oauth2ResourceServer(OAuth2ResourceServerConfigurer::jwt)
                 .httpBasic(Customizer.withDefaults())
                 .build();
+    }
+
+    @Bean
+    DataSource dataSource() throws IOException {
+        return new EmbeddedDatabaseBuilder()
+                .setType(H2)
+                .addScript(Files.readString(Paths.get("src/main/resources/customUsers.ddl")))
+                .build();
+    }
+
+    @Bean
+    UserDetailsManager users(DataSource dataSource) {
+        UserDetails user = User.builder()
+                .username("user")
+                .password(passwordEncoder().encode("1234"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.builder()
+                .username("admin")
+                .password(passwordEncoder().encode("1234"))
+                .roles("USER", "ADMIN")
+                .build();
+        JdbcUserDetailsManager users = new JdbcUserDetailsManager(dataSource);
+        users.createUser(user);
+        users.createUser(admin);
+        return users;
     }
 
 }
